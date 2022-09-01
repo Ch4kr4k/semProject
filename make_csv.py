a7a17de27a4e5f4e7142e7a9bc33429b967b7f46
zera <fluffypnd4247@gmail.com>
2022-09-01 15:54:40 +0530

    Update

--- make_csv.py
+++ make_csv.py
@@ -1,10 +1,17 @@
+import sys
+
 import pandas as pd
 
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
 
-
+if len(sys.argv)==1:
+        file='data.csv'
+        print(len(sys.argv))
+else:
+        file = sys.arv[1]
+        file=file+'.csv'
 #For creating csv file
 def create_csv():
 	col =[]
@@ -12,6 +19,6 @@ def create_csv():
 		col.append(str(mp_hands.HandLandmark(i).name))
 	print(len(col))
 	data = pd.DataFrame(columns=col)
-	data.to_csv('data.csv', index=False)
+	data.to_csv(file, index=False)
 	
 create_csv()
