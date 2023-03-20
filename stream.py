0ffe2336e287592b296ca6ecfcb2b2dacc3cb016
Ch4kr4k <marakmuna9@gmail.com>
2023-03-20 22:38:54 +0530

    update

--- stream.py
+++ stream.py
@@ -18,6 +18,12 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 # Create a gesture recognizer instance with the live stream mode:
 
 
+def key_cmd(gestures1):
+
+    if gestures1 == 'rock':
+        key.press(ges_to_cmd[gestures1])
+
+
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     
     top_gesture = result.gestures
@@ -25,6 +31,8 @@ def print_result(result: GestureRecognizerResult, output_image: mp.Image, timest
 
     if len(top_gesture) == 1:
         res = top_gesture[0][0].category_name
+        res_l = ''
+        res_r = ''
         print(res)
 
     elif len(top_gesture) == 2:
@@ -36,42 +44,48 @@ def print_result(result: GestureRecognizerResult, output_image: mp.Image, timest
     else:
         res = ''
 
+    key_cmd(res)
 
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result,
-    num_hands=2)
 
-with GestureRecognizer.create_from_options(options) as recognizer:
+def main():
+    options = GestureRecognizerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=print_result,
+        num_hands=2)
 
-    cap = cv2.VideoCapture(0)
-    with mp_hands.Hands(
+    with GestureRecognizer.create_from_options(options) as recognizer:
+
+        cap = cv2.VideoCapture(0)
+        with mp_hands.Hands(
             model_complexity=0,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5) as hands:
 
-        while cap.isOpened():
-            success, frame = cap.read()
-            frame_timestamp_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
+            while cap.isOpened():
+                success, frame = cap.read()
+                frame_timestamp_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
 
-            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+                image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-            recognizer.recognize_async(image, int(frame_timestamp_ms))
+                recognizer.recognize_async(image, int(frame_timestamp_ms))
 
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = hands.process(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        frame,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                results = hands.process(frame)
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(
+                            frame,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS,
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style())
 
-            cv2.imshow('rps', cv2.flip(frame, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-    cap.release()
+                cv2.imshow('rps', cv2.flip(frame, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+        cap.release()
+
+if __name__ == '__main__':
+    main()
